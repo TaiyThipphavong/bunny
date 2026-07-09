341cdb1268a595b8d0780201d2b401ba3e2fec3b
Vanidsha TY <youremail@example.com>
2026-07-09 23:36:19 +0700

    Update database connection to use Railway variables

--- api/config.php
+++ api/config.php
@@ -4,10 +4,10 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
 
-$host = 'localhost';
-$dbname = 'bunnies_bunn';
-$user = 'root';
-$pass = '';
+$host = getenv("DB_HOST");
+$dbname = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
