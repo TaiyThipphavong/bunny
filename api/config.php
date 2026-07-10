87e635d8c49f004973f761e8309ffba7b41ba352
Vanidsha TY <youremail@example.com>
2026-07-10 15:32:28 +0700

    edit ok55

--- api/config.php
+++ api/config.php
@@ -7,8 +7,12 @@ $database = "railway";
 $port     = 3306;  // ພອດພາຍໃນຂອງ MySQL ຕ້ອງເປັນ 3306 (ຫ້າມໃຊ້ 57395)
 
 try {
-    $conn = new PDO("mysql:host=$host;port=$port;dbname=$database;charset=utf8", $user, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // $conn = new PDO("mysql:host=$host;port=$port;dbname=$database;charset=utf8", $user, $password);
+    // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // // เปลี่ยนเป็น $pdo ให้ตรงกับโค้ดตัวระบบหลัก
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$database;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch(PDOException $e) {
     // ຫາກເຊື່ອມຕໍ່ບໍ່ຜ່ານ ໃຫ້ມັນພົ່ນ Error ອອກມາເທິງໜ້າຈໍຂາວໆທັນທີ
     die("Database Connection Failed: " . $e->getMessage());
